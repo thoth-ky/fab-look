452876e9afeeb2422528e7d8559c66c1c8a77a56
Mutuku Kyalo <28805113+jmutuku95@users.noreply.github.com>
2019-02-13 09:29:01 +0300

    Graphql spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+    # first_name { Faker::Name.first_name }
+    # last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
 
