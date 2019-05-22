66b3dd35260d0e4259c4f414a17eb8c0d79ca75c
Joseph Mutuku Kyalo <28805113+thoth-ky@users.noreply.github.com>
2019-05-22 15:17:41 +0300

    Update project to use Ruby 2.6 (#9)
    * remove deprecated gems
    
    * update to ruby 2.6.3
    
    * Fix Lint errors

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
- RSpec.describe IndexController, type: :controller do
+RSpec.describe IndexController, type: :controller do
   describe 'GET #index' do
     it 'responds successfully with an HTTP 200 status code' do
       get :index
-      expect(response).to be_success
+      expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
 
-     it 'renders the index template' do
+    it 'renders the index template' do
       get :index
       assert_template('index/index')
     end
